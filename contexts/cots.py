2b59bd63314b506222ee7ba6c515dcafcd7eede8
Mec-iS <tunedconsulting@gmail.com>
2017-06-11 15:45:37 +0100

    Refactor and fix

---
+++ contexts/cots.py
@@ -0,0 +1,19 @@
+# UNFINISHED
+cots_context = {
+  "@context": {
+    "hydraspec": "http://www.w3.org/ns/hydraspec/core#",
+    "vocab": "http://hydrus.com/vocab#",
+    "Cots": "http://ontology.projectchronos.eu/subsystems?format=jsonld",
+# COTS PROPERTIES
+    "name": "http://schema.org/name",
+    # "description": "http://schema.org/description",
+    # "start_date": {
+    #   "@id": "http://schema.org/startDate",
+    #   "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
+    # },
+    # "end_date": {
+    #   "@id": "http://schema.org/endDate",
+    #   "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
+    # }
+  }
+}
---
+++ contexts/entrypoint.py
@@ -0,0 +1,16 @@
+#UNFINISHED
+entrypoint_context = {
+  "@context": {
+    "hydraspec": "http://www.w3.org/ns/hydraspec/core#",
+    "vocab": "http://hydrus.com/vocab#",
+    "EntryPoint": "vocab:EntryPoint",
+    "spacecrafts": {
+      "@id": "vocab:EntryPoint/spacecrafts",
+      "@type": "@id"
+    },
+    "cots": {
+      "@id": "vocab:EntryPoint/cots",
+      "@type": "@id"
+    }
+  }
+}
