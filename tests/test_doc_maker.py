efa574a1160d002b026ab87b82f49812dfd5aab9
aribalam <aribalam64@gmail.com>
2019-02-27 23:43:20 +0530

    created unit tests for class_in_endpoint method

--- tests/test_doc_maker.py
+++ tests/test_doc_maker.py
@@ -59,3 +59,29 @@ class TestCreateClass(unittest.TestCase):
             "description": False,
             "supportedOperation": False
         }
+
+
+class TestClassInEndPoint(unittest.TestCase):
+
+    def setUp(self):
+        self.doc = hydra_doc_sample.doc
+
+    def test_validations(self):
+
+        class_dict = self.doc["supportedClass"][0]
+        entrypoint = doc_maker.get_entrypoint(self.doc)
+
+        # check if proper exception is raised when supportedProperty key is not present in entrypoint
+        properties = entrypoint.pop("supportedProperty")
+        self.assertRaises(SyntaxError, doc_maker.class_in_endpoint, class_dict, entrypoint)
+
+        # check if proper exception is raised when property key is not present
+        property_ = properties[0].pop("property")
+        entrypoint["supportedProperty"] = properties
+        self.assertRaises(SyntaxError, doc_maker.class_in_endpoint, class_dict, entrypoint)
+
+        # check if exception is raised when no label key is found in property
+        properties[0]["property"] = property_
+        label = property_.pop("label")
+        self.assertRaises(SyntaxError, doc_maker.collection_in_endpoint, class_dict, entrypoint)
+        property_["label"] = label
