88ca75ca723299f1ed18fbfce2160580d25f028f
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2021-05-29 10:17:03 +0100

    Bump to version 0.4

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ try:
     # pip >=20
     from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    install_requires = parse_requirements('requirements.txt', session=PipSession())
-    dependencies = [str(package.requirement) for package in install_requires]
 except ImportError:
     try:
         # 10.0.0 <= pip <= 19.3.1
@@ -16,12 +14,15 @@ except ImportError:
         from pip.download import PipSession
         from pip.req import parse_requirements
 
-    install_requires = parse_requirements('requirements.txt', session=PipSession())
+install_requires = parse_requirements('requirements.txt', session=PipSession())
+try:
+    dependencies = [str(package.requirement) for package in install_requires]
+except:
     dependencies = [str(package.req) for package in install_requires]
 
 setup(
     name='hydra_python_core',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     license='MIT',
     description='Core functions for Hydrus',
