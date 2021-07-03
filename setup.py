fe18d88ceb375be4675bb89cddc5096eab27b675
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2021-07-03 15:39:11 +0100

    Bump to version 0.3.1

--- setup.py
+++ setup.py
@@ -5,14 +5,8 @@ try:
     from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
 except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
+    msg = "Your pip version is out of date, please run `pip install --upgrade pip setuptools`"
+    raise ImportError(msg)
 
 install_requires = parse_requirements('requirements.txt', session=PipSession())
 try:
@@ -22,7 +16,7 @@ except:
 
 setup(
     name='hydra_python_core',
-    version='0.3',
+    version='0.3.1',
     packages=find_packages(),
     license='MIT',
     description='Core functions for Hydrus',
